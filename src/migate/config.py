4ea64e27b2a0f368b07da8570efffa1f18995a53
offici5l <127665988+offici5l@users.noreply.github.com>
2026-04-28 20:29:22 +0100

    Update config.py

--- src/migate/config.py
+++ src/migate/config.py
@@ -1,15 +1,15 @@
 from rich.console import Console
 from rich.theme import Theme
 
-BASE_URL              = "https://account.xiaomi.com"
-SERVICELOGIN_URL      = BASE_URL + "/pass/serviceLogin"
+BASE_URL = "https://account.xiaomi.com"
+SERVICELOGIN_URL = BASE_URL + "/pass/serviceLogin"
 SERVICELOGINAUTH2_URL = SERVICELOGIN_URL + "Auth2"
-LIST_URL              = BASE_URL + "/identity/list"
-SEND_EM_TICKET        = BASE_URL + "/identity/auth/sendEmailTicket"
-SEND_PH_TICKET        = BASE_URL + "/identity/auth/sendPhoneTicket"
-VERIFY_EM             = BASE_URL + "/identity/auth/verifyEmail"
-VERIFY_PH             = BASE_URL + "/identity/auth/verifyPhone"
-USERQUOTA_URL         = BASE_URL + "/identity/pass/sms/userQuota"
+LIST_URL = BASE_URL + "/identity/list"
+SEND_EM_TICKET = BASE_URL + "/identity/auth/sendEmailTicket"
+SEND_PH_TICKET = BASE_URL + "/identity/auth/sendPhoneTicket"
+VERIFY_EM = BASE_URL + "/identity/auth/verifyEmail"
+VERIFY_PH = BASE_URL + "/identity/auth/verifyPhone"
+USERQUOTA_URL = BASE_URL + "/identity/pass/sms/userQuota"
 REGION_URL = BASE_URL + "/pass/user/login/region"
 REGIONCONFIG_URL = BASE_URL + "/pass2/config"
 LONGPOLLING_URL = BASE_URL + "/longPolling/loginUrl"
